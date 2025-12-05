b585f6563ca11d3a9876e77d882a2e60a0af38b6
tobijaotieno-hue <tobijaotieno@gmail.com>
2025-12-05 10:13:32 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Guido")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(45, 55)
+
 
 def halve(number):
-    pass
+    return number / 2 
+halve(100)
